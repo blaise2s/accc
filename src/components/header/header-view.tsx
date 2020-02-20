6e6d1bec029e11673f0febf1213bdf625bcfad8a
blaise2s <blaise.schaeffer@gmail.com>
2020-02-20 10:51:54 -0500

    feat(HEADER): Header nav styling

--- src/components/header/header-view.tsx
+++ src/components/header/header-view.tsx
@@ -5,9 +5,24 @@ import styles from "./header-view.module.scss";
 import logo from "../../images/ACCC-Logo.png";
 
 const Header: React.FC = () => {
+  const clickHandler = () => console.log("TODO: Use click handler...");
+
+  const navItems = ["Schedule", "Results", "Rules", "News", "Leadership"];
+
   return (
     <div className={styles.header}>
-      <img className={styles.logo} src={logo} alt="ACCC Logo"></img>
+      <img onClick={clickHandler} className={styles.logo} src={logo} alt="ACCC Logo"></img>
+      <nav>
+        <ul className={styles.navigation}>
+          {navItems.map((navItem: string) => {
+            return (
+              <li key={navItem} className={styles.navItem}>
+                <div>{navItem}</div>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
     </div>
   );
 };
